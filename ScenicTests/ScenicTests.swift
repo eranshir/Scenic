7d00c72c2df00ffa3b84a0ac36bd7422deba1517
eranshir <eran.shir@gmail.com>
2025-08-12 19:11:53 +0300

    Initial Commit

---
+++ ScenicTests/ScenicTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScenicTests.swift
+//  ScenicTests
+//
+//  Created by Eran Shir on 12/8/25.
+//
+
+import Testing
+@testable import Scenic
+
+struct ScenicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
