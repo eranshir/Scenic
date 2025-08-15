7bcda63e4b5eeceefdbd1e92aa47e97a0758c909
eranshir <eran.shir@gmail.com>
2025-08-16 01:27:01 +0300

    Initial Commit

---
+++ Scenic/ScenicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScenicApp.swift
+//  Scenic
+//
+//  Created by Eran Shir on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ScenicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
